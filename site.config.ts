c0cc78606dd63b845959e221eb32bd27c3967e49
SabaGPT <128382419+SabaGPT@users.noreply.github.com>
2023-12-17 22:07:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config' 
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3ab1378bc2fe42368456a7187c63ac5e', 
+  rootNotionPageId: '4ae59ad416a94dadb3a428f85364d516',  
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -24,7 +24,7 @@ export default siteConfig({
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX` 
 
-  // default notion icon and cover images for site-wide consistency (optional)
+  // default notion icon and cover images for site-wide consistency (optional)                                                                                                                                                                                     
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
   defaultPageCover: null,
@@ -48,7 +48,7 @@ export default siteConfig({
   // }
   pageUrlOverrides: null,
 
-  // whether to use the default notion navigation style or a custom one with links to 
+  // whether to use the default notion navigation style or a custom one with links to  
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`. 
   navigationStyle: 'default'
   // navigationStyle: 'custom',
